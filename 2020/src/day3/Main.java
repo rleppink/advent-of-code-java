92399b25dcaf11012196c0efef7b6f85687bcecf
Ronald Leppink <ronaldleppink@gmail.com>
2020-12-03 11:00:34 +0100

    Greatly simplify 2020/day 3
    I anticipated a harder exercise, which is why I parsed everything into a neat grid of enum-named things. And that wasn't necessary at all.

--- 2020/src/day3/Main.java
+++ 2020/src/day3/Main.java
@@ -10,52 +10,28 @@ public class Main {
     Lib.runDay(3, Main::parseFile, Main::part1, Main::part2);
   }
   
-  public static Square[][] parseFile(String input) {
-    String[] lines = input.split("\n");
-    Square[] firstLine = parseLine(lines[0]);
-    Square[][] grid = new Square[lines.length][firstLine.length];
-    grid[0] = firstLine;
-    for (int i = 1; i < lines.length; i++) {
-      grid[i] = parseLine(lines[i]);
-    }
-    
-    return grid;
+  public static String[] parseFile(String input) {
+    return input.split("\n");
   }
   
-  public static int part1(Square[][] grid) {
-    return traverse(grid, 3, 1);
+  public static int part1(String[] lines) {
+    return traverse(lines, 3, 1);
   }
   
-  public static long part2(Square[][] grid) {
+  public static long part2(String[] lines) {
     return
       (long)
-      traverse(grid, 1, 1) *
-      traverse(grid, 3, 1) *
-      traverse(grid, 5, 1) *
-      traverse(grid, 7, 1) *
-      traverse(grid, 1, 2);
+      traverse(lines, 1, 1) *
+      traverse(lines, 3, 1) *
+      traverse(lines, 5, 1) *
+      traverse(lines, 7, 1) *
+      traverse(lines, 1, 2);
   }
   
-  enum Square {
-    Open,
-    Tree
-  }
-  
-  private static Square[] parseLine(String line) {
-    Square[] squares = new Square[line.length()];
-    for (int i = 0; i < line.length(); i++) {
-      squares[i] = line.charAt(i) == ".".charAt(0)
-        ? Square.Open
-        : Square.Tree;
-    }
-    
-    return squares;
-  }
-  
-  private static int traverse(Square[][] grid, int xStep, int yStep) {
+  private static int traverse(String[] lines, int xStep, int yStep) {
     int trees = 0;
-    for (int x = 0, y = 0; y < grid.length; x += xStep, y += yStep) {
-      if (grid[y][x % grid[0].length] == Square.Tree) 
+    for (int x = 0, y = 0; y < lines.length; x += xStep, y += yStep) {
+      if (lines[y].charAt(x % lines[0].length()) == "#".charAt(0))
         trees++;
     }
     
